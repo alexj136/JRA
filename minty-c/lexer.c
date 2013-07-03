dd4d076e11b5e1400b3cbce02e2d62593abcdfb3
Alex Jeffery <apj21@sussex.ac.uk>
2013-07-03 22:44:43 +0100

    Added a file 'lexer.c' and Makefile for the c implementation in minty-c/

---
+++ minty-c/lexer.c
@@ -0,0 +1,10 @@
+/*
+This file contains functions that are used for lexical analysis.
+*/
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	printf("arg[1] = %c\n", *argv[1]);
+	return 0;
+}
