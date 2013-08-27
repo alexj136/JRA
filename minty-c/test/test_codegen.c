50fa6a18f7f5c3e9b9ca96b867ca2e4f28c4a957
Alex Jeffery <apj21@sussex.ac.uk>
2013-08-27 17:25:25 +0100

    Continued jitter & assembly code generator implementation

--- minty-c/test/test_codegen.c
+++ minty-c/test/test_codegen.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <malloc.h>
 #include "minunit.h"
 #include "../minty_util.h"
@@ -7,7 +8,91 @@
 #include "../parser.h"
 #include "../codegen.h"
 
+/*
+ * Macro that writes the given string to a file with a given name
+ */
+#define WRITE(name, string) \
+	do { \
+		FILE *fp = fopen(name, "w"); \
+		if(!fp) { \
+			printf("Could not open file: '"#name"'\n"); \
+			exit(EXIT_FAILURE); \
+		} \
+		fprintf(fp, "%s\n", string); \
+		if(fclose(fp)) { \
+			printf("Error closing file: '"#name"'\n"); \
+			exit(EXIT_FAILURE); \
+		} \
+	} while(0)
+
+/*
+ * Macro that removes a file with the given name
+ */
+#define REMOVE(name) \
+	do { \
+		if(system("rm "#name"") == -1) { \
+			printf("Failed to remove '%s'\n", name); \
+			exit(EXIT_FAILURE); \
+		} \
+	} while(0)
+
+/*
+ * Function that builds a source file with the given name, and gives the output
+ * file a separate name that is also given as a parameter
+ */
+void build(char *src, char *out) {
+	
+	char *command = (char *)safe_alloc(sizeof(char) * 100);
+	sprintf(command, "gcc %s -o %s", src, out);
+	
+	int success = system(command) == 0;
+	free(command);
+
+	if(!success) {
+		printf("Failed to build '%s' as '%s'\n", src, out);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/*
+ * Function that runs an executable of the given name and checks that the result
+ * of its execution was appropriate. Since the function used to execute shell
+ * commands returns -1 to indicate errors, the expected result cannot be -1, or
+ * we would not know if -1 was returned as the correct output of the program or
+ * if -1 was returned because an error occured
+ */
+bool checked_run(char *file, int expected_result) {
+	if(expected_result == -1) {
+		printf("Cannot execute '%s' with expected result of -1\n", file);
+		exit(EXIT_FAILURE);
+	}
+	
+	char *command = (char *)safe_alloc(sizeof(char) * 100);
+	sprintf(command, "./%s", file);
+	
+	int command_result = system(command);
+	if(command_result == -1) {
+		printf("Warning: checked_run(%s, %d) call returned -1\n",
+			file, expected_result);
+	}
+
+	free(command);
+	return command_result == expected_result;
+}
+
 int tests_run = 0;
+
+char *test_file_io() {
+
+	WRITE("twelve.s", " .globl main \n main: \n movl $12, %eax \n ret \n");
+	build("twelve.s", "twelve");
+	int success = checked_run("twelve", 12);
+	REMOVE("twelve");
+	REMOVE("twelve.s");
+	mu_assert(success, "test_file_io failed");
+
+	return NULL;
+}
 
 char *test_ternary() {
 
@@ -82,18 +167,8 @@ char *test_large_expression() {
 	return NULL;
 }
 
-char *all_tests() {
-	// mu_run_test(test_ternary);
-	// mu_run_test(test_large_expression);
-
-	return NULL;
-}
-
-RUN_TESTS(all_tests);
-
-/*
-int main() {
-	LinkedList *tokens = lex("fn main(x) { return fibonacci(x); }\
+char *test_fibonacci() {
+	LinkedList *tokens = lex("fn main() { return fibonacci(6); }\
 	fn fibonacci(x) {\
 		if x = 0 {\
 			return 0;\
@@ -108,11 +183,29 @@ int main() {
 
 	Program *ast = parse_program(tokens);
 	char *asm_prog = codegen_program(ast);
-	// printf("%s\n", asm_prog);
+	
+	WRITE("test/fib.s", asm_prog);
+	build("test/fib.s", "test/fib");
+	int success = checked_run("test/fib", 8);
+	REMOVE("test/fib");
+	REMOVE("test/fib.s");
+	mu_assert(success, "test_fibonacci failed");
+
 	free(asm_prog);
 	Program_free(ast);
 	LLMAP(tokens, Token *, Token_free);
 	LinkedList_free(tokens);
-	return 0;
+
+	return NULL;
 }
-*/
+
+char *all_tests() {
+	mu_run_test(test_file_io);
+	// mu_run_test(test_ternary);
+	// mu_run_test(test_large_expression);
+	mu_run_test(test_fibonacci);
+
+	return NULL;
+}
+
+RUN_TESTS(all_tests);
